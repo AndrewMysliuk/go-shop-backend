434d06bdbe57da608e18d7f7f914055dfe36983c
Andrii Mysliuk <andrii.mysliuk@omg.one>
2022-07-01 14:03:57 +0300

    hendler auth tests

--- internal/handler/products_test.go
+++ internal/handler/products_test.go
@@ -1,23 +1,23 @@
 package handler
 
-import "testing"
+// import "testing"
 
-func TestHandler_createProduct(t *testing.T) {
-	// TODO
-}
+// func TestHandler_createProduct(t *testing.T) {
+// 	// TODO
+// }
 
-func TestHandler_getAllProducts(t *testing.T) {
-	// TODO
-}
+// func TestHandler_getAllProducts(t *testing.T) {
+// 	// TODO
+// }
 
-func TestHandler_getProductById(t *testing.T) {
-	// TODO
-}
+// func TestHandler_getProductById(t *testing.T) {
+// 	// TODO
+// }
 
-func TestHandler_updateProduct(t *testing.T) {
-	// TODO
-}
+// func TestHandler_updateProduct(t *testing.T) {
+// 	// TODO
+// }
 
-func TestHandler_deleteProduct(t *testing.T) {
-	// TODO
-}
+// func TestHandler_deleteProduct(t *testing.T) {
+// TODO
+// }
